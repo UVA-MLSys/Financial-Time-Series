b22211c39b16e1e1de20bd1af146741843b1bd6b
Md. Khairul Islam <khairulislamtanim@gmail.com>
2024-09-01 23:43:12 -0400

    fix micn and timemixer

--- scripts/slurm/exchange_rate.sh
+++ scripts/slurm/exchange_rate.sh
@@ -15,15 +15,31 @@ module load cuda-toolkit cudnn-8.9.5_cuda12.x anaconda3
 conda deactivate
 conda activate ml
 
-models=("DLinear" "MICN" "SegRNN" "iTransformer" "PatchTST" "TimeMixer" "TimesNet")
+models=(DLinear PatchTST TimesNet iTransformer)
+data_path=Exchange_Rate_Report.csv
+n_features=7
+itrs=3
 
 for model in ${models[@]}
 do 
 echo "Running for model:$model"
 
 python run.py \
-    --n_features 7 \
-    --data_path Exchange_Rate_Report.csv\
-    --model $model --disable_progress --itrs 3
-
+    --n_features $n_features \
+    --data_path $data_path\
+    --model $model --itrs $itrs --disable_progress
 done
+
+# MICN requires label_len to be equal to seq_len
+python run.py \
+    --n_features $n_features \
+    --data_path $data_path\
+    --model MICN --disable_progress --itrs $itrs --label_len 96
+
+python run.py\
+    --model TimeMixer\
+    --n_features $n_features --data_path $data_path\
+    --down_sampling_layers 3 --down_sampling_window 2\
+    --d_model 16 --d_ff 32 --label_len 0 \
+    --down_sampling_method avg --e_layers 3 \
+    --factor 3 --channel_independence 1 --itrs $itrs  --disable_progress
