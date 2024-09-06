850f2646e78dfc17a92029c54281d0e86c9b3424
khairulislam <khairulislamtanim@gmail.com>
2024-09-06 16:52:34 -0400

    remove incomplete results

--- scripts/Financial_Aid.sh
+++ scripts/Financial_Aid.sh
@@ -6,6 +6,9 @@ seq_len=5
 label_len=3
 pred_len=1
 itrs=3
+top_k=2
+patch_len=4
+stride=2
 target=OFFER_BALANCE
 
 for model in ${models[@]}
@@ -17,7 +20,7 @@ python run.py \
     --data_path $data_path\
     --model $model --itrs $itrs\
     --seq_len $seq_len --label_len $label_len --pred_len $pred_len\
-    --target $target
+    --target $target --top_k $top_k --freq a
 done
 
 # MICN requires label_len to be equal to seq_len
@@ -26,7 +29,7 @@ python run.py \
     --data_path $data_path\
     --model MICN --disable_progress --itrs $itrs\
     --seq_len $seq_len --label_len $seq_len --pred_len $pred_len\
-    --target $target
+    --target $target --freq a
 
 python run.py\
     --model TimeMixer\
@@ -34,7 +37,7 @@ python run.py\
     --down_sampling_layers 3 --down_sampling_window 2\
     --d_model 16 --d_ff 32\
     --seq_len $seq_len --label_len 0 --pred_len $pred_len\
-    --down_sampling_method avg --e_layers 3 \
+    --down_sampling_method avg --e_layers 3 --freq a\
     --factor 3 --channel_independence 1 --itrs $itrs  --features $features\
     --target $target
 
@@ -42,7 +45,7 @@ python run_CALF.py\
     --n_features $n_features --features $features \
     --data_path $data_path\
     --itrs $itrs\
-    --model_id ori --d_model 768\
+    --model_id ori --freq a --d_model 768\
     --seq_len $seq_len --label_len $label_len --pred_len $pred_len\
     --target $target
 
@@ -50,14 +53,14 @@ python run_OFA.py\
     --n_features $n_features --features $features \
     --data_path $data_path\
     --itrs $itrs --d_model 768\
-    --model_id ori --patch_size 4 --stride 2\
+    --model_id ori --patch_size $patch_len --stride $stride\
     --seq_len $seq_len --label_len $label_len --pred_len $pred_len\
     --target $target
 
 python run_TimeLLM.py\
     --n_features $n_features --d_model 16\
-    --data_path $data_path \
+    --data_path $data_path --freq a \
     --batch_size 16 --itrs $itrs --disable_progress\
     --seq_len $seq_len --label_len $label_len --pred_len $pred_len\
-    --model_id ori --target $target --top_k 2\
-    --patch_len 4 --stride 2
+    --model_id ori --target $target --top_k $top_k\
+    --patch_size $patch_len --stride $stride
