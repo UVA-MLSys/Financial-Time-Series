03722f90cc90a3f23d05a84d79e40b21fa72732c
khairulislam <khairulislamtanim@gmail.com>
2024-10-03 11:24:46 -0400

    add varying percent few shot results for aid and exchange data

---
+++ scripts/few_shot/few_shot_vary_exchange.sh
@@ -0,0 +1,53 @@
+models=(DLinear PatchTST TimesNet iTransformer)
+data_path=Exchange_Rate_Report.csv
+n_features=7
+itrs=3
+percents=(25 50 75)
+
+for percent in ${percents[@]}
+do
+    for model in ${models[@]}
+    do 
+    echo "Running for model:$model"
+
+    python run.py \
+        --n_features $n_features \
+        --data_path $data_path\
+        --model $model --itrs $itrs --disable_progress --percent $percent
+    done
+
+    # MICN requires label_len to be equal to seq_len
+    python run.py \
+        --n_features $n_features \
+        --data_path $data_path\
+        --model MICN --disable_progress --itrs $itrs\
+        --label_len 96 --percent $percent
+
+    python run.py\
+        --model TimeMixer\
+        --n_features $n_features --data_path $data_path\
+        --down_sampling_layers 3 --down_sampling_window 2\
+        --d_model 16 --d_ff 32 --label_len 0 \
+        --down_sampling_method avg --e_layers 3 \
+        --factor 3 --channel_independence 1 --itrs $itrs\
+        --disable_progress --percent $percent
+
+    python run_CALF.py\
+        --n_features $n_features --d_model 768\
+        --data_path $data_path\
+        --itrs $itrs --disable_progress\
+        --model_id ori --percent $percent
+
+    python run_OFA.py\
+        --n_features $n_features \
+        --data_path $data_path\
+        --itrs $itrs --disable_progress --d_model 768\
+        --model_id ori --batch_size 16 --percent $percent
+
+    python run_TimeLLM.py\
+        --n_features $n_features --d_model 16\
+        --data_path $data_path \
+        --batch_size 16 --itrs $itrs --disable_progress\
+        --model_id ori --percent $percent
+
+done
